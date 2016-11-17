12bd686abb5ef0058af69409329b0f201d81fc49
zhixuxin <924277637@qq.com>
2016-11-17 19:48:00 +0800

    2016/11/17

--- System/UI/MasterPageone.master.cs
+++ System/UI/MasterPageone.master.cs
@@ -15,48 +15,130 @@ public partial class MasterPageone : System.Web.UI.MasterPage
         Label2.Text = Session["Role"].ToString();
         Label3.Text = Session["CurrentWeek"].ToString();
         Label4.Text = "当前在线" + Application["online"].ToString() + "人";
+
         if (!IsPostBack)
         {
-            //DataTable dt = getDT("select city from city where judge=1");
-            string SQL = "select city from tb_TreeView_Info where judge=1";
-            DataTable dt = Class1.getDT(SQL);
 
-            TreeNode Tn1 = new TreeNode();
-            Tn1.Text = "管理员面板";
-            TreeView1.Nodes.Add(Tn1);
-
-            for (int i = 0; i < dt.Rows.Count; i++)
+            if (Session["Role"].ToString() == "系统管理员")
             {
 
-                TreeNode tn1 = new TreeNode();
-                tn1.Text = dt.Rows[i][0].ToString();
-                Tn1.ChildNodes.Add(tn1);
-                String SQL1 = "select city from tb_TreeView_Info where bianhao=" + (i + 2) + " and judge=2";
-                DataTable dt1 = Class1.getDT(SQL1);
-                //  DataTable dt1 = getDT("select city from city where bianhao=" + (i + 2) + " and judge=2");
+                string SQL = "select city from tb_TreeView_Info where judge=1";
+                DataTable dt = Class1.getDT(SQL);
 
-                for (int j = 0; j < dt1.Rows.Count; j++)
+                TreeNode Tn1 = new TreeNode();
+                Tn1.Text = "管理员面板";
+                TreeView1.Nodes.Add(Tn1);
+
+                for (int i = 0; i < dt.Rows.Count; i++)
                 {
 
-                    TreeNode tn2 = new TreeNode();
-                    tn2.Text = dt1.Rows[j][0].ToString();
+                    TreeNode tn1 = new TreeNode();
+                    tn1.Text = dt.Rows[i][0].ToString();
+                    Tn1.ChildNodes.Add(tn1);
+                    String SQL1 = "select city from tb_TreeView_Info where bianhao=" + (i + 2) + " and judge=2";
+                    DataTable dt1 = Class1.getDT(SQL1);
+                    //  DataTable dt1 = getDT("select city from city where bianhao=" + (i + 2) + " and judge=2");
 
-                    tn1.ChildNodes.Add(tn2);
+                    for (int j = 0; j < dt1.Rows.Count; j++)
+                    {
+
+                        TreeNode tn2 = new TreeNode();
+                        tn2.Text = dt1.Rows[j][0].ToString();
+
+                        tn1.ChildNodes.Add(tn2);
+                    }
+                }
+            }
+
+            else if (Session["Role"].ToString() == "院系领导")
+            {
+                TreeNode TN1 = new TreeNode();
+                TN1.Text = "管理面板";
+                TreeView1.Nodes.Add(TN1);
+
+
+                string SQL = "select city from city where judge=11";
+                DataTable dt = Class1.getDT(SQL);
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    TreeNode tn1 = new TreeNode();
+                    tn1.Text = dt.Rows[i][0].ToString();
+                    TN1.ChildNodes.Add(tn1);
+                    string SQL1 = "select city from city where bianhao=" + (i + 8) + "and judge=2";
+                    DataTable dt1 = Class1.getDT(SQL1);
+                    for (int j = 0; j < dt1.Rows.Count; j++)
+                    {
+
+                        TreeNode tn2 = new TreeNode();
+                        tn2.Text = dt1.Rows[j][0].ToString();
+                        tn1.ChildNodes.Add(tn2);
+                    }
+                }
+            }
+            else if (Session["Role"].ToString() == "学管人员")
+            {
+                string SQL = "select city from city where judge=22";
+                DataTable dt = Class1.getDT(SQL);
+
+                TreeNode Tn1 = new TreeNode();
+                Tn1.Text = "管理面板";
+                TreeView1.Nodes.Add(Tn1);
+
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+
+                    TreeNode tn1 = new TreeNode();
+                    tn1.Text = dt.Rows[i][0].ToString();
+                    Tn1.ChildNodes.Add(tn1);
+                    String SQL1 = "select city from city where bianhao=" + (i + 11) + " and judge=2";
+                    DataTable dt1 = Class1.getDT(SQL1);
+                    //  DataTable dt1 = getDT("select city from city where bianhao=" + (i + 2) + " and judge=2");
+
+                    for (int j = 0; j < dt1.Rows.Count; j++)
+                    {
+
+                        TreeNode tn2 = new TreeNode();
+                        tn2.Text = dt1.Rows[j][0].ToString();
+
+                        tn1.ChildNodes.Add(tn2);
+                    }
+                }
+            }
+            else if (Session["Role"].ToString() == "教师")
+            {
+                string SQL = "select city from city where judge=33";
+                DataTable dt = Class1.getDT(SQL);
+
+                TreeNode Tn1 = new TreeNode();
+                Tn1.Text = "教师面板";
+                TreeView1.Nodes.Add(Tn1);
+
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+
+                    TreeNode tn1 = new TreeNode();
+                    tn1.Text = dt.Rows[i][0].ToString();
+                    Tn1.ChildNodes.Add(tn1);
+                    String SQL1 = "select city from city where bianhao=" + (i + 14) + " and judge=2";
+                    DataTable dt1 = Class1.getDT(SQL1);
+
+
+                    for (int j = 0; j < dt1.Rows.Count; j++)
+                    {
+
+                        TreeNode tn2 = new TreeNode();
+                        tn2.Text = dt1.Rows[j][0].ToString();
+
+                        tn1.ChildNodes.Add(tn2);
+                    }
                 }
             }
         }
+
+
+
     }
-    //public static DataTable getDT(string SQL)
-    //{
-    //    string strConn = "data source=.;initial catalog=Treeview;uid=sa;password=sa";
-    //    SqlConnection conn = new SqlConnection(strConn);
-    //    conn.Open();
-    //    DataTable dt = new DataTable();
-    //    SqlDataAdapter da = new SqlDataAdapter(SQL, conn);
-    //    da.Fill(dt);
-    //    conn.Close();
-    //    return dt;
-    //}
+   
     protected void TreeView1_SelectedNodeChanged(object sender, EventArgs e)
     {
         if (TreeView1.SelectedNode.Text == "本校教师")
@@ -75,11 +157,13 @@ public partial class MasterPageone : System.Web.UI.MasterPage
         {
             Response.Redirect("ChangePWD.aspx");
         }
+    }
 
         
-    }
+    
     protected void Button1_Click(object sender, EventArgs e)
     {
         
     }
 }
+
--- System/UI/mubanye4.aspx
+++
@@ -1,5 +0,0 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPagefour.master" AutoEventWireup="true" CodeFile="mubanye4.aspx.cs" Inherits="mubanye4" %>
-
-<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder2" Runat="Server">
-</asp:Content>
-
